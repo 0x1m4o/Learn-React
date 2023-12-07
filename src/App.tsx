a517c87d5c4f00160b527abcb0b419de4e5af3b6
0x1m4o <chuntdevnx@gmail.com>
2023-12-07 13:29:12 +0700

    passing data from children

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import "./App.css";
 // import ListGroup from "./components/ListGroup";
-import ListGroupWithProps from "./components/ListGroupWithProps";
-
+// import ListGroupWithProps from "./components/ListGroupWithProps";
+import Alert from "./components/Alert";
 function App() {
-  let allCountry = ["Indonesia", "Japan", "United States", "Russia"];
+  // let allCountry = ["Indonesia", "Japan", "United States", "Russia"];
 
   return (
     <div>
       {/* <ListGroup /> */}
-      <ListGroupWithProps
+      {/* <ListGroupWithProps
         title="ListGroupWithProps"
         allCountry={allCountry}
         onSelected={(item) => {
           console.log(item);
         }}
-      />
+      /> */}
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
